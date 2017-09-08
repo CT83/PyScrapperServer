0e340238185d70e6aa1843020545dcd5fa977650
RohanSawant <rohansawantct83@gmail.com>
2017-09-08 20:53:51 +0530

    Added Responsive Download Page

--- BottleServer.py
+++ BottleServer.py
@@ -4,7 +4,6 @@ from Scrapper import main, book_name
 
 webpage = """
 <html>
-<meta http-equiv="refresh" content="5;URL=/">
 <style>
 form {
     border: 3px solid #f1f1f1;
@@ -75,20 +74,22 @@ span.psw {
     <input type="text" placeholder="Enter the Link of the Book" name="url" required>
     <label><b>Website</b></label>
     <select name="website">
-    <option value="0" selected>(please select:)</option>
-    <option value="readromancebook.com">www.readromancebook.com</option>
+    <option value="0">(please select:)</option>
+    <option value="readromancebook.com" selected>www.readromancebook.com</option>
     <option value="2">two</option>
     <option value="3">three</option>
     <option value="other">other, please specify:</option>
     </select>
     
-    <button value="Download" type="submit">Login</button>
+    <button value="Download" type="submit">Begin Scraping</button>
   </div>
  </form>
 </body>
 </html>
 
 """
+
+
 @get('/')  # or @route('/login')
 def homepage():
     return webpage
@@ -103,11 +104,61 @@ def result():
 def download():
     url = request.forms.get('url')
     website = request.forms.get('website')
-    file = main(url,website)
-    print("Submitted URL:"+url)
-    print("Submitted Website:"+website)
-    return template('download_status', name='Rohan')
-    return "<a href='/static/out/" + file + "'>Download</a>"
+    file = main(url, website)
+    print("Submitted URL:" + url)
+    print("Submitted Website:" + website)
+    # return template('download_status', name='Rohan')
+    download_webpage = '''
+    <html>
+    <style>
+    form {
+        border: 3px solid #f1f1f1;
+    }
+
+    button {
+        background-color: #4CAF50;
+        color: white;
+        padding: 14px 20px;
+        margin: 8px 0;
+        border: none;
+        cursor: pointer;
+        width: 100%;
+    }
+
+    button:hover {
+        opacity: 0.8;
+    }
+
+    .container {
+        padding: 16px;
+    }
+
+    span.psw {
+        float: right;
+        padding-top: 16px;
+    }
+
+    /* Change styles for span and cancel button on extra small screens */
+    @media screen and (max-width: 300px) {
+        span.psw {
+           display: block;
+           float: none;
+        }
+     }
+    </style>
+    <body>
+
+    <h2>Bottle Scrapper</h2>
+    <form action=/static/out/''' + file + '''>
+      <div class="container">
+        <button value="Download" type="submit">Download</button>
+      </div>
+     </form>
+    </body>
+    </html>
+    '''
+    return download_webpage
+    # return "<a href='/static/out/" + file + "'>Download</a>"
 
 
 @route('/static/out/<filename>')
